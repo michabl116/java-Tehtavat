296eb9220e220b89e498c9008e1bb18afc9e77ca
michabl116 <michabl@metropolia.fi>
2025-03-18 14:41:51 +0200

    start java

---
+++ 1.1_Moduli/src/Task_2.java
@@ -0,0 +1,15 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        int num = 5;
+
+        int num1 = 5;
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+    }
+}
